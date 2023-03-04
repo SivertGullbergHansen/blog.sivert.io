dd034d296ddd6e80530a18eb90112c358e9e4943
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-03-04 17:00:11 +0100

    Adjust exit stagger speed

--- config/animations.ts
+++ config/animations.ts
@@ -1,6 +1,6 @@
 const exitAnimation = { opacity: 0, y: -5, transition: { duration: .33, type: 'spring' } }
 const transition = { type: 'spring', stiffness: 200 }
-export const parentArticleTransition = { staggerChildren: .01, type: 'spring' }
+export const parentArticleTransition = { staggerChildren: .015, type: 'spring' }
 
 export const ArticleCardVariant = {
     initial: { opacity: .0001, x: -50 },
