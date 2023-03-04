cb78ad5877eaf468e2444aa977d4a8cf22254bad
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-03-04 16:28:02 +0100

    Speeeed

--- config/animations.ts
+++ config/animations.ts
@@ -1,17 +1,15 @@
 const exitAnimation = { opacity: 0, y: -5, transition: { duration: .33, type: 'spring' } }
 const transition = { type: 'spring', stiffness: 200 }
-export const parentArticleTransition = { staggerChildren: .05, type: 'spring' }
+export const parentArticleTransition = { staggerChildren: .01, type: 'spring' }
 
 export const ArticleCardVariant = {
     initial: { opacity: .0001, x: -50 },
-    animate: { opacity: 1, x: 0 },
+    animate: { opacity: 1, x: 0, transition: transition },
     exit: exitAnimation,
-    transition: transition
 }
 
 export const ArticleContentVariant = {
     initial: { opacity: .0001, y: -50 },
-    animate: { opacity: 1, y: 0 },
+    animate: { opacity: 1, y: 0, transition: transition },
     exit: exitAnimation,
-    transition: transition
 }
