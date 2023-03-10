8a7ca5fafb638509eadac83d0ba53b27bc630d02
Sivert <saiverx@gmail.com>
2023-03-10 01:47:39 +0100

    Update header transparency

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,14 @@ import { motion } from "framer-motion"
 export function Header({headerPadding}: {headerPadding: number}) {
   
   return (
-    <motion.header style={{transition: 'all ease .25s'}} className={`bg-base-300 z-50 flex flex-row p-${headerPadding} ${headerPadding === 0 && '2xl:bg-opacity-0'} bg-opacity-100 place-items-center justify-center lg:justify-start fixed top-0 left-0 right-0`}>
+    <motion.header style={{transition: 'all ease .25s'}} className={`bg-base-300 z-50 flex flex-row p-${headerPadding} ${headerPadding === 0 && 'xl:bg-opacity-0'} bg-opacity-100 place-items-center justify-center xl:justify-start fixed top-0 left-0 right-0`}>
       <Link scroll={false} href="/" className="btn btn-ghost normal-case font-bold">
         Sivert Gullberg Hansen
       </Link>
-      <nav className="flex flex-row gap-4 fixed top-0 right-0 lg:relative">
+      <nav className="flex flex-row gap-4 fixed top-0 right-0 xl:relative">
         <Link href="https://sivert.io" className="flex gap-1 btn btn-ghost">
           <IoDocumentText className="w-4 h-4" />
-          <span className="lg:block hidden">Portfolio</span>
+          <span className="xl:block hidden">Portfolio</span>
         </Link>
       </nav>
     </motion.header>
