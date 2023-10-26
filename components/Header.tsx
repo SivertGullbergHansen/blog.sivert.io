df6d0334f268101243bcf256c572f4c1199c3cd4
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-10-26 15:26:13 +0200

    Add scroll false to tag links

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { allPosts } from "contentlayer/generated";
-import { useRouter } from 'next/router'
-import { IoLogoGithub } from 'react-icons/io5'
+import { useRouter } from "next/router";
+import { IoLogoGithub } from "react-icons/io5";
 
 export function Header({
   headerPadding,
@@ -11,28 +11,27 @@ export function Header({
 }: {
   headerPadding: string;
   maxWidth: string;
-  }) {
-    const router = useRouter()
-  const [allTags, setallTags] = useState<string[]>([])
-  const [showTags, setshowTags] = useState(false)
+}) {
+  const router = useRouter();
+  const [allTags, setallTags] = useState<string[]>([]);
+  const [showTags, setshowTags] = useState(false);
   useEffect(() => {
-    const t = []
-  // Loop through each post and extract its tags
-  allPosts.forEach((post) => {
-    if (post.tags && Array.isArray(post.tags)) {
-      // Add each tag to the allTags array
-      post.tags.forEach((tag) => !t.includes(tag) && t.push(tag))
-    }
-  });
-    t.sort()
-  setallTags(t)
-  }, [])
+    const t = [];
+    // Loop through each post and extract its tags
+    allPosts.forEach((post) => {
+      if (post.tags && Array.isArray(post.tags)) {
+        // Add each tag to the allTags array
+        post.tags.forEach((tag) => !t.includes(tag) && t.push(tag));
+      }
+    });
+    t.sort();
+    setallTags(t);
+  }, []);
 
   useEffect(() => {
     setshowTags(false);
-  }, [router])
-  
-  
+  }, [router]);
+
   return (
     <motion.header
       style={{ transition: "all ease .25s" }}
@@ -44,8 +43,22 @@ export function Header({
       >
         <motion.div
           style={{ transition: "all ease .25s" }}
-          className={`absolute left-0 right-0 -bottom-12 bg-base-100 rounded-lg shadow p-1 flex items-center justify-end gap-1 ${showTags ? 'pointer-events-auto opacity-100 translate-y-0' : 'pointer-events-none opacity-0 -translate-y-4'}`}>
-          {allTags.map((tag) => <Link key={tag} className="capitalize btn btn-ghost btn-sm" href={`/tags/${tag}`}>{tag}</Link>)}
+          className={`absolute left-0 right-0 -bottom-12 bg-base-100 rounded-lg shadow p-1 flex items-center justify-end gap-1 ${
+            showTags
+              ? "pointer-events-auto opacity-100 translate-y-0"
+              : "pointer-events-none opacity-0 -translate-y-4"
+          }`}
+        >
+          {allTags.map((tag) => (
+            <Link
+              key={tag}
+              scroll={false}
+              className="capitalize btn btn-ghost btn-sm"
+              href={`/tags/${tag}`}
+            >
+              {tag}
+            </Link>
+          ))}
         </motion.div>
         <Link
           scroll={false}
@@ -55,9 +68,18 @@ export function Header({
           Sivert Gullberg Hansen
         </Link>
         <nav className="flex gap-2">
-          <button className="btn btn-ghost normal-case" onClick={() => setshowTags(!showTags)}>Tags</button>
-          <Link target="_blank" href="https://github.com/SivertGullbergHansen" className="flex gap-1 btn btn-ghost">
-            <IoLogoGithub className="h-4 w-4"/>
+          <button
+            className="btn btn-ghost normal-case"
+            onClick={() => setshowTags(!showTags)}
+          >
+            Tags
+          </button>
+          <Link
+            target="_blank"
+            href="https://github.com/SivertGullbergHansen"
+            className="flex gap-1 btn btn-ghost"
+          >
+            <IoLogoGithub className="h-4 w-4" />
           </Link>
         </nav>
       </motion.div>
